40c159c55a647854162abc3b5cff912faafdbcee
Paweł Skorupski <oxyaego98@gmail.com>
2021-01-01 03:33:03 +0100

    hotfix

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ const port = 5000;
 
 app.use(express.static("public"));
 
-app.listen(port, () => console.log(`Server running at port ${port}`));
+app.listen(process.env.PORT || 5000, () => console.log(`Server running at port ${port}`));
